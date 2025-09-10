4ab24751c4c37a4451ddd274caf8f0864468b4c5
lukasluut <lukasluut@gmail.com>
2025-09-10 17:09:27 -0300

    Feat: adicionado imagem de perfil

--- quizSenhorDosAneis/src/pages/Profile.jsx
+++ quizSenhorDosAneis/src/pages/Profile.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Profile.css'
+import windowPerfil from '../assets/images/window.png'
+
 
 function Profile() {
   return (
     <div className='body-container-profile'>
-        <div className='profile-container'>  
-            <div className='stone-container'>
-                </div>          
+        <div className='profile-container'> 
+          <div className='portrait-container'>
+            <img className='window-perfil' src={windowPerfil} alt="foto de perfil" />
+          </div> 
+          <div className='stone-container'>
+
+          </div>          
         </div>
 
     </div>
