9ca393a64640c9df63021f372420a907a8f0e632
LukasLuut <lukasluut@gmail.com>
2025-09-11 02:56:11 -0300

    Feat: adicionado mapa a tela de jogo

--- quizSenhorDosAneis/src/components/Map.jsx
+++ quizSenhorDosAneis/src/components/Map.jsx
@@ -3,16 +3,16 @@ import "leaflet/dist/leaflet.css";
 import mapImage from "../assets/images/map.webp";
 import L from "leaflet";
 
-// Ícones customizados (coloque suas imagens em /assets/icons)
+// Ícones customizados
 import mordorIconUrl from "../assets/icons/mordor.png";
 import shireIconUrl from "../assets/icons/shire.png";
 import rivendellIconUrl from "../assets/icons/rivendell.png";
 
 export default function MapaTerraMedia() {
-  // Tamanho da imagem em pixels (ajuste conforme sua imagem real)
+  // Tamanho da imagem em pixels 
   const bounds = [
     [0, 0], // canto inferior esquerdo
-    [4344, 5000], // altura x largura (exemplo 2000px de largura e 1000px de altura)
+    [4344, 5000], // altura x largura 
   ];
 
   // Criando ícones customizados
@@ -41,24 +41,24 @@ export default function MapaTerraMedia() {
     <MapContainer
       crs={L.CRS.Simple}
         bounds={bounds}
-        maxBounds={bounds}        // 🚫 não deixa arrastar além da borda da imagem
-        maxBoundsViscosity={1.0}  // "cola" a borda (0 = livre, 1 = totalmente restrito)
-        style={{ height: "100vh", width: "100%" }}
-        minZoom={-1}
+        maxBounds={bounds}        // não deixa arrastar além da borda da imagem
+        maxBoundsViscosity={0}  // "cola" a borda (0 = livre, 1 = totalmente restrito)
+        style={{ height: "100%", width: "100%" }}
+        minZoom={-2.5}
     >
       <ImageOverlay url={mapImage} bounds={bounds} />
 
       {/* Marcadores temáticos */}
       <Marker position={[3600, 4200]} icon={mordorIcon}>
-        <Popup>Mordor 🔥 – “Um Anel para a todos governar...”</Popup>
+        <Popup>Mordor: “Um Anel para a todos governar...”</Popup>
       </Marker>
 
       <Marker position={[300, 300]} icon={shireIcon}>
-        <Popup>Condado 🌿 – Lar dos Hobbits!</Popup>
+        <Popup>Condado: Lar dos Hobbits!</Popup>
       </Marker>
 
       <Marker position={[500, 1200]} icon={rivendellIcon}>
-        <Popup>Rivendell ⛰️ – O refúgio dos Elfos</Popup>
+        <Popup>Rivendell: O refúgio dos Elfos</Popup>
       </Marker>
     </MapContainer>
   );
--- quizSenhorDosAneis/src/pages/Profile.jsx
+++ quizSenhorDosAneis/src/pages/Profile.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import './Profile.css'
 import windowPerfil from '../assets/images/window.png'
+import Map from '../components/Map'
 
 
 function Profile() {
   return (
     <div className='body-container-profile'>
         <div className='profile-container'> 
+
           <div className='portrait-container'>
             <img className='window-perfil' src={windowPerfil} alt="foto de perfil" />
           </div> 
-          <div className='stone-container'>
 
-          </div>          
+          <div className='stone-container'></div>  
+
+        </div>
+        <div className='map-container'>
+          <Map></Map>
         </div>
 
     </div>
