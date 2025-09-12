e561a024e58d4f0598ca49a3a7483b188a33d0ce
LukasLuut <lukasluut@gmail.com>
2025-09-12 02:19:12 -0300

    Feat: adicionado botões e outros compontentes visuais

--- quizSenhorDosAneis/src/pages/Profile.jsx
+++ quizSenhorDosAneis/src/pages/Profile.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Profile.css'
 import windowPerfil from '../assets/images/window.png'
 import Map from '../components/Map'
+import bgVideo2 from "../assets/videos/the-shire.mp4";
+import flag from "../assets/images/scroll.png"
 
 
 function Profile() {
   return (
     <div className='body-container-profile'>
-        <div className='profile-container'> 
+       {/* <div className='profile-container'> 
 
           <div className='portrait-container'>
             <img className='window-perfil' src={windowPerfil} alt="foto de perfil" />
@@ -18,9 +20,18 @@ function Profile() {
         </div>
         <div className='map-container'>
           <Map></Map>
-        </div>
+        </div>*/}
+        <div className='profile-container'>
+          <div className='flag-profile'>
+            <img className='flagg' src={flag} alt="" />
+          </div>
 
+        </div>
+        <video className="bg-video" autoPlay muted loop playsInline >
+        <source src={bgVideo2} type="video/mp4" />
+      </video>
     </div>
+    
   )
 }
 
