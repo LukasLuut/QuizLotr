8fafc96a2fd35e891f19c253205f141323c01daf
LukasLuut <lukasluut@gmail.com>
2025-09-13 20:56:48 -0300

    Fix: ajustado botão start que não recebia efeito de hover, adicionado efeitos

--- quizSenhorDosAneis/src/pages/Start.jsx
+++ quizSenhorDosAneis/src/pages/Start.jsx
@@ -4,23 +4,44 @@ import circleOneRing from '../assets/images/circleOneRing.png'
 import circleOneRing2 from '../assets/images/circleOneRing2.png'
 import lordOfTheRings from '../assets/images/lordOfTheRings.png'
 
-
 function Start() {
   return (
     <div className='bg'>
-      <div class="loader__container">
-        <img src={lordOfTheRings} alt=" title: Lord of the Rings" />
-        <div class="loader__ring">
-          <img class="loader__inscription" src={circleOneRing} alt="The One Ring Inscription" />
-          <img class="loader__inscription loader__inscription2" src={circleOneRing2} alt="The One Ring Inscription" />
+      <div className="loader__container">
+        {/*Aqui e o titulo*/}
+        <img src={lordOfTheRings} alt="Lord of the Rings" />
+
+
+        <div className='rings'>
+            <div className="loader__ring">
+              {/*Aqui estão as imagens da inscrição em elfico*/}
+              <img className="loader__inscription" src={circleOneRing} alt="The One Ring Inscription" />
+              <img className="loader__inscription loader__inscription2" src={circleOneRing2} alt="The One Ring Inscription" />
+            </div>
+
+              {/*Aqui estou criando um circulo*/}
+              <svg viewBox="0 0 300 300">
+              <defs>
+                <path
+                  id="circlePath"
+                  d="
+                    M 150, 150
+                    m -120,0
+                    a 120,120 0 1,1 240,0
+                    a 120,120 0 1,1 -240,0
+                  "
+                />
+              </defs>
+                {/*Aqui está o texto que ficará no circulo */}
+              <text className="circle-text">
+                <textPath href="#circlePath" startOffset="50%" textAnchor="middle" textLength="750" lengthAdjust="spacingAndGlyphs">
+                  "One Ring to rule them all, One Ring to find them, One Ring to bring them all, and in the darkness bind them."
+                </textPath>
+              </text>
+            </svg>
+            <h2 className='btn-start'>INICIAR</h2>
         </div>
-
-        <h2 className='btn-start'>INICIAR</h2>
-
       </div>
-
-
-
     </div>
   )
 }
