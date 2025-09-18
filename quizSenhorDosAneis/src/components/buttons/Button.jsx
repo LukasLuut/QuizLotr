5dfa55246f6eb5fb6244fbfaac7a2164859b8678
ReisVC <vitor.reisvc@gmail.com>
2025-09-18 17:07:52 -0300

    feat: implementado sistema de registro e login

--- quizSenhorDosAneis/src/components/buttons/Button.jsx
+++ quizSenhorDosAneis/src/components/buttons/Button.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import './Button.css'
 
 function Button(props) {
+  // let handle = props.handleEntrarClick();
+
+
   return (
-    <button type="submit" className="btn">
+    <button type="submit" className="btn" onClick={(e) => {
+      e.preventDefault();
+      if(props.texto == "Entrar") { 
+        props.handleEntrarClick();
+      } else {
+        props.handleRegistrarClick();
+      }}}>
             <p>{props.texto}</p>
           </button>
   )
--- quizSenhorDosAneis/src/pages/Login.jsx
+++ quizSenhorDosAneis/src/pages/Login.jsx
@@ -5,52 +5,137 @@ import { useState } from "react";
 
 function Login() {
   const [mostrarEmail, setMostrarEmail] = useState(false);
-  
-  return (
-  <div className="login-body">
-    <div className="login-page">
-      
-      {/* Aqui o Formulário */}
-      <div className="form-container ">
-        <div className="title-container ">
-           <h2 className="title-login">Quiz da Terra-médiA</h2>
-        </div>
-       
-        <form>
-          
-          {/* Input de User*/}
-          <div className="form-group">            
-            <label htmlFor="username">Nome de usuário</label>
-            <input type="text" id="username" placeholder="Ex: Frodo"/>
-          </div>
+  const [mostrarBtnEntrar, setMostrarBtnEntrar] = useState(false);
+  const [mostrarBtnRegistrar, setMostrarBtnRegistrar] = useState(true);
+  let btnTitle = "Entrar"
 
-          {/* Input de Email*/}          
-            {mostrarEmail && (
+
+  const handleEntrarClick = async () => {
+    let email = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    try {
+      const res = await fetch(`http://localhost:3000/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json" // Corrigido: "Content-Type", não "content-Type"
+        },
+        body: JSON.stringify({
+          email: email, // Substitua com o valor real, se necessário
+          password: password
+        })
+      });
+
+      const data = await res.json();
+
+      if(data.token) {
+        alert("VOCÊ LOGOU")
+      } else {
+        alert("ERRRROOOOU")
+      }
+
+      const token = data.token
+      localStorage.setItem("token", token)
+      console.log(token)
+
+    } catch (err) {
+      console.error("Erro:", err);
+    }
+  }
+
+  const handleRegistrarClick = async () => {
+      let name = document.getElementById("username").value;
+      let email = document.getElementById("email").value;
+      let password = document.getElementById("password").value;
+
+      try {
+        const res = await fetch(`http://localhost:3000/auth/register`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json" // Corrigido: "Content-Type", não "content-Type"
+          },
+          body: JSON.stringify({
+            name: name,
+            email: email, // Substitua com o valor real, se necessário
+            password: password
+          })
+        });
+
+        const data = await res.json();
+
+        console.log(data)
+      } catch (err) {
+        console.error("Erro:", err);
+      }
+
+    }
+
+    if (!mostrarEmail) {
+      const bt = document.getElementById("btnEntrar")
+    }
+
+    if (mostrarBtnEntrar) {
+    }
+
+    if (mostrarBtnEntrar) {
+      btnTitle = "Registrar"
+    }
+
+
+
+
+    return (
+      <div className="login-body">
+        <div className="login-page">
+
+          {/* Aqui o Formulário */}
+          <div className="form-container ">
+            <div className="title-container ">
+              <h2 className="title-login">Quiz da Terra-médiA</h2>
+            </div>
+
+            <form>
+
+              {/* Input de User*/}
               <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder="Ex: frodo@shire.me"/>
+                <label htmlFor="username">Nome de usuário</label>
+                <input type="text" id="username" placeholder="Ex: Frodo" />
               </div>
+
+              {/* Input de Email*/}
+              {mostrarEmail && (
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" id="email" placeholder="Ex: frodo@shire.me" />
+                </div>
+              )}
+
+              {/* Input de senha*/}
+              <div className="form-group">
+                <label htmlFor="password">Senha</label>
+                <input type="password" id="password" placeholder="••••••••" />
+              </div>
+
+              <Button texto={btnTitle} handleEntrarClick={handleEntrarClick} handleRegistrarClick={handleRegistrarClick} />
+            </form>
+
+            {mostrarBtnRegistrar && (
+              <a href="#" className="link" id="btnRegistrar" onClick={() => { setMostrarBtnEntrar(true); setMostrarBtnRegistrar(false); setMostrarEmail(true) }}>Criar nova conta</a>
             )}
 
-          {/* Input de senha*/}
-          <div className="form-group">
-            <label htmlFor="password">Senha</label>
-            <input type="password" id="password" placeholder="••••••••" />
+            {mostrarBtnEntrar && (
+              <a href="#" className="link" id="btnEntrar" onClick={() => { setMostrarBtnEntrar(false); setMostrarBtnRegistrar(true); setMostrarEmail(false); btnTitle = "Registrar" }}>Entrar</a>
+            )}
+
           </div>
 
-          <Button texto="Entrar"/>
-        </form>
-
-        <a href="#" className="link" onClick={() => setMostrarEmail(true)}>Criar nova conta</a>
+          {/* vídeo de plano de fundo */}
+          <video className="bg-video" autoPlay muted loop playsInline >
+            <source src={bgVideo} type="video/mp4" />
+          </video>
+        </div>
       </div>
+    )
+  }
 
-      {/* vídeo de plano de fundo */}
-      <video className="bg-video" autoPlay muted loop playsInline >
-        <source src={bgVideo} type="video/mp4" />
-      </video>
-    </div>
-  </div>
-  )
-}
-
-export default Login;
+  export default Login;
